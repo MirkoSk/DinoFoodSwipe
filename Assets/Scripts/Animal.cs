1e69a3ea2bdf72580d4c12ba4f17e2ef5e6f6d13
Mirko <mirkoskroch@gmail.com>
2021-06-08 09:49:26 +0200

    Dino Switching

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -1,16 +1,103 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 
+
+[System.Serializable]
+public class Dino
+{
+    public string Name;
+    public Sprite Sprite;
+}
 
 public class Animal : MonoBehaviour
 {
     [SerializeField] Diet diet;
+    [SerializeField] bool dinoFacingLeft;
+    [SerializeField] float moveOutDelay = 3f;
+    [SerializeField] float moveOutEndValue;
+    [SerializeField] float moveOutDuration = 1f;
+    [SerializeField] Ease moveOutEase = Ease.InOutQuad;
+
+    [Space]
+    [SerializeField] List<Dino> dinos = new List<Dino>();
+
+    [Header("References")]
     [SerializeField] Transform speechBubblePosition;
-    [SerializeField] bool speechBubbleFacingRight;
+    [SerializeField] SpriteRenderer dinoSpriteRenderer;
+
+    Dino currentDino;
 
     public Diet Diet { get { return diet; } }
     public Transform SpeechBubblePosition { get => speechBubblePosition; }
-    public bool SpeechBubbleFacingRight { get => speechBubbleFacingRight; }
+    public bool DinoFacingLeft { get => dinoFacingLeft; }
+
+
+
+    private void Start()
+    {
+        SwitchAnimal(0f, false);
+    }
+
+    private void OnEnable()
+    {
+        GameEvents.FoodGiven += HandleFoodRecieved;
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.FoodGiven -= HandleFoodRecieved;
+    }
+
+
+
+    void HandleFoodRecieved(int points, Animal animalFed, FoodType foodGiven)
+    {
+        // only act if this animal was fed
+        if (animalFed != this) return;
+
+        if (foodGiven.Diet == diet)
+        {
+            SwitchAnimal();
+        }
+    }
+
+    void SwitchAnimal(float delay = -1f, bool tweenOut = true)
+    {
+        float tweeningStart = dinoSpriteRenderer.transform.localPosition.x;
+        
+        float tweeningEndValue = 0f;
+        if (dinoFacingLeft) tweeningEndValue = moveOutEndValue;
+        else tweeningEndValue = -moveOutEndValue;
+        
+        float tweenDelay = 0f;
+        if (delay == -1f) tweenDelay = moveOutDelay;
+        else tweenDelay = delay;
+
+        float tweenOutDuration = 0f;
+        if (tweenOut) tweenOutDuration = moveOutDuration;
+        else tweenOutDuration = 0f;
+
+        dinoSpriteRenderer.transform.DOLocalMoveX(tweeningEndValue, tweenOutDuration).SetEase(moveOutEase).SetDelay(tweenDelay).OnComplete(() =>
+        {
+            SwitchCurrentDino();
+
+            dinoSpriteRenderer.transform.DOLocalMoveX(tweeningStart, moveOutDuration).SetEase(moveOutEase);
+        });
+    }
+
+    void SwitchCurrentDino()
+    {
+        List<Dino> alternateDinos = new List<Dino>();
+        alternateDinos.AddRange(dinos);
+        if (currentDino != null) alternateDinos.Remove(currentDino);
+
+        Dino newDino = alternateDinos[Random.Range(0, alternateDinos.Count)];
+
+        dinoSpriteRenderer.sprite = newDino.Sprite;
+
+        currentDino = newDino;
+    }
 }
--- Assets/Scripts/SpeechBubbles.cs
+++ Assets/Scripts/SpeechBubbles.cs
@@ -9,6 +9,7 @@ public class SpeechBubbles : MonoBehaviour
     [Space]
     [SerializeField] float popupScaleDuration = 0.3f;
     [SerializeField] Ease easingCurve = Ease.Linear;
+    [SerializeField] float showDuration = 3f;
 
     [Header("References")]
     [SerializeField] GameObject rightAnswerSpeechBubble;
@@ -23,13 +24,11 @@ public class SpeechBubbles : MonoBehaviour
     private void OnEnable()
     {
         GameEvents.FoodGiven += ShowSpeechBubble;
-        GameEvents.ScoringEnded += HideSpeechBubbles;
     }
 
     private void OnDisable()
     {
         GameEvents.FoodGiven -= ShowSpeechBubble;
-        GameEvents.ScoringEnded -= HideSpeechBubbles;
     }
 
     void ShowSpeechBubble(int points, Animal animalFed, FoodType foodGiven)
@@ -40,12 +39,12 @@ public class SpeechBubbles : MonoBehaviour
             rightAnswerSpeechBubble.transform.localScale = Vector3.zero;
             rightAnswerSpeechBubble.transform.position = animalFed.SpeechBubblePosition.position;
 
-            if (animalFed.SpeechBubbleFacingRight)
+            if (animalFed.DinoFacingLeft)
             {
                 rightAnswerSpeechBubble.GetComponentInChildren<TextMeshPro>().transform.localScale = new Vector3(1f, 1f, 1f);
                 rightAnswerSpeechBubble.transform.DOScale(1f, popupScaleDuration).SetEase(easingCurve);
             }
-            else if (!animalFed.SpeechBubbleFacingRight)
+            else if (!animalFed.DinoFacingLeft)
             {
                 rightAnswerSpeechBubble.GetComponentInChildren<TextMeshPro>().transform.localScale = new Vector3(-1f, 1f, 1f);
                 rightAnswerSpeechBubble.transform.DOScale(new Vector3(-1f, 1f, 1f), popupScaleDuration).SetEase(easingCurve);
@@ -57,21 +56,25 @@ public class SpeechBubbles : MonoBehaviour
             wrongAnswerSpeechBubble.transform.localScale = Vector3.zero;
             wrongAnswerSpeechBubble.transform.position = animalFed.SpeechBubblePosition.position;
 
-            if (animalFed.SpeechBubbleFacingRight)
+            if (animalFed.DinoFacingLeft)
             {
                 wrongAnswerSpeechBubble.GetComponentInChildren<TextMeshPro>().transform.localScale = new Vector3(1f, 1f, 1f);
                 wrongAnswerSpeechBubble.transform.DOScale(1f, popupScaleDuration).SetEase(easingCurve);
             }
-            else if (!animalFed.SpeechBubbleFacingRight)
+            else if (!animalFed.DinoFacingLeft)
             {
                 wrongAnswerSpeechBubble.GetComponentInChildren<TextMeshPro>().transform.localScale = new Vector3(-1f, 1f, 1f);
                 wrongAnswerSpeechBubble.transform.DOScale(new Vector3(-1f, 1f, 1f), popupScaleDuration).SetEase(easingCurve);
             }
         }
+
+        StartCoroutine(HideSpeechBubbles());
     }
 
-    void HideSpeechBubbles(int totalPoints = 0)
+    IEnumerator HideSpeechBubbles()
     {
+        yield return new WaitForSeconds(showDuration);
+
         rightAnswerSpeechBubble.SetActive(false);
         wrongAnswerSpeechBubble.SetActive(false);
     }
